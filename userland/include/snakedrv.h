8b9125f2877bf46d7d153d7913500c4075b7a441
cybersnakeh <maillot6633@gmail.com>
2026-06-04 19:21:18 +0200

    release: emit sigstore protobuf bundles

--- userland/include/snakedrv.h
+++ userland/include/snakedrv.h
@@ -25,8 +25,8 @@
 
 #define SNAKEDRV_VERSION_MAJOR      2
 #define SNAKEDRV_VERSION_MINOR      0
-#define SNAKEDRV_VERSION_PATCH      1
-#define SNAKEDRV_VERSION_STRING     "2.0.1"
+#define SNAKEDRV_VERSION_PATCH      2
+#define SNAKEDRV_VERSION_STRING     "2.0.2"
 
 /*
  * Wire-compatibility ABI version.
